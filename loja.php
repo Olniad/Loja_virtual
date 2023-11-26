0b8169ef9f12bf1f8657e0ad61ec2d0433801672
Danilo <danilo.martins333326@gmail.com>
2023-11-26 19:26:59 -0300

    update

---
+++ loja.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loja.css">
+    <link rel="shortcut icon" type="imagex/png" href="lulu.ico">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap');</style>
+    <script src="main.js"></script>
+    <title>KASSAKISMURF</title>
+</head>
+<body>
+<div class="header">
+        <div class="divTable">
+        <th><img class="img logo" src="lulu.ico"></a></th> 
+        <th>
+            <table>
+              <thead>               
+                <tr>                  
+                  <th class="font-header"><a class="linkzudo" href="#">KASSAKISMURF'S</a></th>
+                  
+                  <th class="font-header"><a class="linkzudo" href="https://github.com/olniad" target="_blank">Problemas no site?</a></th>
+                  
+                  <th class="font-header"><a class="linkzudo" href="https://github.com/olniad" target="_blank">Compras e vendas</a></th>
+                  
+                  <th class="font-header"><a class="linkzudo" href="https://github.com/olniad" target="_blank">Políticas</a></th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+</div>
+<!-- fazer um dialogo-->
+<div class=content2>
+<div class="content">
+<form>
+<h1 class="formulario">SMURF BÁSICA.</h1>
+<hr class="linha">
+<h3 class="formulario">Escolher skin.</h3>
+<h3 class="formulario">30K de essências azuis.</h3>
+<h3 class="formulario">Sem proteção a banimentos.</h3>
+<h3 class="formulario">Upada por bot.</h3>
+<h3 class="formulario">R$5,00</h3>
+<button class="button">COMPRAR</button>
+</form> 
+</div>
+<!-- fazer um dialogo-->
+<div class="content_premium">
+<form>
+<h1 class="formulario_premium">SMURF PREMIUM.</h1>
+<span id="linha"><hr></span>
+<h3 class="formulario_premium">Escolher skin.</h3>
+<h3 class="formulario_premium">60K de essências azuis.</h3>
+<h3 class="formulario_premium">Proteção a banimentos.</h3>
+<h3 class="formulario_premium">Upada a mão.</h3>
+<h3 class="formulario_premium">R$20,00</h3>
+<button class="button_premium">COMPRAR</button>
+</form> 
+</div>
+</div>
+</body>
